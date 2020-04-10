46fd5f0a7a176e661d14711ca1f8464c1969a003
Ben Levitt <benjie@gmail.com>
2020-04-09 17:45:56 -0700

    Add another compile-time helper for glade weirdness

---
+++ src/GtkComboBoxEntryText.hh
@@ -0,0 +1,10 @@
+//
+//    Glade doesn't know about class ComboBoxText...
+//
+//    (If you know/figure out a way to clean this up, please tell me!!)
+//
+
+#include <gtkmm/comboboxentrytext.h>
+
+
+#define GtkComboBoxEntryText Gtk::ComboBoxEntryText
